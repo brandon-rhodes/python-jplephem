1deeb609ba6a20d4aa4daa7570bf7e5a52c6cc6c
Brandon Rhodes <brandon@rhodesmill.org>
2012-03-30 12:07:12 -0400

    Made setup.py work for "jplephem" too, and gave it a "long description" for PyPI.

--- jplephem/__init__.py
+++ jplephem/__init__.py
@@ -1,3 +1,37 @@
-"""Package supporting JPL planetary ephemeris computations."""
+"""Package supporting JPL planetary ephemeris computations.
 
+This package lets you consult a Jet Propulsion Laboratory ephemeris for
+the position and velocity of one of the planets, or the magnitude and
+rate-of-change of the Earth's nutation or the Moon's libration.  To
+determine the position of Mars, for example::
+
+    import de423
+    from jplephem import Ephemeris
+
+    e = Ephemeris(de423)
+    jed = 2444391.5  # 1980.06.01
+    print e.compute('mars', jed)
+
+The result should be a tuple providing the object's position in the
+Solar System given in kilometers, as well as its velocity in kilometers
+per second::
+
+    (x, y, z, xrate, yrate, zrate)
+
+The ephemerides currently available as Python packages (the following
+links explain the differences between them) are:
+
+* `DE405 <http://pypi.python.org/pypi/de421>`_ (May 1997)
+* `DE406 <http://pypi.python.org/pypi/de421>`_ (May 1997)
+* `DE421 <http://pypi.python.org/pypi/de421>`_ (February 2008)
+* `DE422 <http://pypi.python.org/pypi/de421>`_ (September 2009)
+* `DE423 <http://pypi.python.org/pypi/de421>`_ (February 2010)
+
+Any of these ephemerides, as well as ``jplephem`` itself, can be
+installed with a simple ``pip`` command::
+
+    pip install jplephem
+    pip install de423
+
+"""
 from .ephem import Ephemeris
--- setup.py
+++ setup.py
@@ -26,22 +26,23 @@ else:
 
 # The ephemeris packages are built very simply.
 
-if name.startswith('d'):
-    module = __import__(name)
-    description, long_description = module.__doc__.split('\n', 1)
-    setup(name = name,
-          version = versions[name],
-          description = description,
-          long_description = long_description,
-          license = 'MIT',
-          author = 'Brandon Rhodes',
-          author_email = 'brandon@rhodesmill.org',
-          classifiers = [
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Topic :: Scientific/Engineering :: Astronomy',
-            ],
-          packages = [name],
-          package_data = {name: ['*.npy',]},
-          )
+module = __import__(name)
+description, long_description = module.__doc__.split('\n', 1)
+
+setup(name = name,
+      version = versions[name],
+      description = description,
+      long_description = long_description,
+      license = 'MIT',
+      author = 'Brandon Rhodes',
+      author_email = 'brandon@rhodesmill.org',
+      classifiers = [
+        'Development Status :: 5 - Production/Stable' if name.startswith('d')
+        else 'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        ],
+      packages = [name],
+      package_data = {name: ['*.npy',]},
+      )
