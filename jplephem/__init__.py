dba90332a7960845619d9db834327f2a787480d9
Brandon Rhodes <brandon@rhodesmill.org>
2012-04-01 12:19:10 -0400

    Corrected the jplephem documentation so that it uses DE421 as its example (as that is the only ephemeris small enough that PyPI has accepted it for upload), and so that the links for the five ephemerides actually point at five different links (whoops).

--- jplephem/__init__.py
+++ jplephem/__init__.py
@@ -3,18 +3,18 @@
 This package lets you consult a Jet Propulsion Laboratory ephemeris for
 the position and velocity of one of the planets, or the magnitude and
 rate-of-change of the Earth's nutation or the Moon's libration.  To
-determine the position of Mars using the DE423 ephemeris, for example,
+determine the position of Mars using the DE421 ephemeris, for example,
 you would start by installing two packages::
 
     pip install jplephem
-    pip install de423
+    pip install de421
 
 Then you can compute positions using a script like this::
 
-    import de423
+    import de421
     from jplephem import Ephemeris
 
-    e = Ephemeris(de423)
+    e = Ephemeris(de421)
     jed = 2444391.5  # 1980.06.01
     print e.compute('mars', jed)
 
@@ -27,11 +27,11 @@ per second::
 The ephemerides currently available as Python packages (the following
 links explain the differences between them) are:
 
-* `DE405 <http://pypi.python.org/pypi/de421>`_ (May 1997)
-* `DE406 <http://pypi.python.org/pypi/de421>`_ (May 1997)
+* `DE405 <http://pypi.python.org/pypi/de405>`_ (May 1997)
+* `DE406 <http://pypi.python.org/pypi/de406>`_ (May 1997)
 * `DE421 <http://pypi.python.org/pypi/de421>`_ (February 2008)
-* `DE422 <http://pypi.python.org/pypi/de421>`_ (September 2009)
-* `DE423 <http://pypi.python.org/pypi/de421>`_ (February 2010)
+* `DE422 <http://pypi.python.org/pypi/de422>`_ (September 2009)
+* `DE423 <http://pypi.python.org/pypi/de423>`_ (February 2010)
 
 """
 from .ephem import Ephemeris
