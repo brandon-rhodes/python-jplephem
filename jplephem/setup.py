ff7fa26cee2fb19ecf5a2b59c8359de63ac5e50f
Brandon Rhodes <brandon@rhodesmill.org>
2013-01-18 11:30:35 -0500

    Clean up metadata for 1.0 release

--- jplephem/setup.py
+++ jplephem/setup.py
@@ -1,28 +1,21 @@
 from distutils.core import setup
 
-versions = {'jplephem': '0.1', 'de405': '1997', 'de406': '1997',
-            'de421': '2008', 'de422': '2009', 'de423': '2010'}
-
-# Fake the presence of numpy so __import__() always succeeds.
+# Fake the presence of numpy so import can succeed.
 import sys
 sys.modules['numpy'] = sys.modules['sys']
 
-name = 'jplephem'
-module = __import__(name)
-description, long_description = module.__doc__.split('\n', 1)
+import jplephem
+description, long_description = jplephem.__doc__.split('\n', 1)
 
-setup(name = name,
-      version = versions[name],
+setup(name = 'jplephem',
+      version = '1.0',
       description = description,
       long_description = long_description,
       license = 'MIT',
       author = 'Brandon Rhodes',
       author_email = 'brandon@rhodesmill.org',
-      url = ('http://jplephem.s3.amazonaws.com/packages.html'
-             if name.startswith('d') else None),
       classifiers = [
-        'Development Status :: 5 - Production/Stable' if name.startswith('d')
-        else 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
@@ -31,7 +24,6 @@ setup(name = name,
         'Programming Language :: Python :: 3.2',
         'Topic :: Scientific/Engineering :: Astronomy',
         ],
-      packages = [name],
-      package_data = {name: ['*.npy',]},
+      packages = ['jplephem'],
       install_requires = ['numpy'],
       )
