9e3315237758c5b74632709c6cbde1be8a5399f8
Brandon Rhodes <brandon@rhodesmill.org>
2013-01-18 02:05:58 -0500

    Switch to smaller DE421 ephemeris for unit tests

--- jplephem/jplephem/test.py
+++ jplephem/jplephem/test.py
@@ -17,47 +17,47 @@ class Tests(TestCase):
 
     def check0(self, x, y, z, dx, dy, dz):
         eq = partial(self.assertAlmostEqual, delta=1.0)
-        eq(x, -3.84258043e+07)
-        eq(y, 1.29508901e+08)
-        eq(z, 5.62536412e+07)
-        eq(dx, -2.52115204e+06)
-        eq(dy, -6.40613144e+05)
-        eq(dz, -2.78642593e+05)
+        eq(x, 39705023.28)
+        eq(y, 131195345.65)
+        eq(z, 56898495.41)
+        eq(dx, -2524248.19)
+        eq(dy, 619970.11)
+        eq(dz, 268928.26)
 
     def check1(self, x, y, z, dx, dy, dz):
         eq = partial(self.assertAlmostEqual, delta=1.0)
-        eq(x, -1.07003862e+08)
-        eq(y, 9.16969864e+07)
-        eq(z, 3.98134914e+07)
-        eq(dx, -1.79386097e+06)
-        eq(dy, -1.73625791e+06)
-        eq(dz, -7.54793163e+05)
+        eq(x, -144692624.00)
+        eq(y, -32707965.14)
+        eq(z, -14207167.26)
+        eq(dx, 587334.38)
+        eq(dy, -2297419.36)
+        eq(dz, -996628.74)
 
     def test_scalar_input(self):
-        import de405
-        e = Ephemeris(de405)
+        import de421
+        e = Ephemeris(de421)
 
-        self.check0(*e.compute('earthmoon', 2305447.5, True))
-        self.check1(*e.compute('earthmoon', 2305478.5, True))
+        self.check0(*e.compute('earthmoon', 2414994.0, True))
+        self.check1(*e.compute('earthmoon', 2415112.5, True))
 
     def test_array_input(self):
-        import de405
-        e = Ephemeris(de405)
+        import de421
+        e = Ephemeris(de421)
 
-        v = e.compute('earthmoon', np.array([2305447.5, 2305478.5]), True)
+        v = e.compute('earthmoon', np.array([2414994.0, 2415112.5]), True)
 
         v = np.array(v)
         self.check0(*v[:,0])
         self.check1(*v[:,1])
 
     def test_too_early_date(self):
-        import de405
-        e = Ephemeris(de405)
+        import de421
+        e = Ephemeris(de421)
         self.assertRaises(IndexError, e.compute, 'earthmoon', e.jalpha - 0.01)
 
     def test_too_late_date(self):
-        import de405
-        e = Ephemeris(de405)
+        import de421
+        e = Ephemeris(de421)
         self.assertRaises(IndexError, e.compute, 'earthmoon', e.jomega + 0.01)
 
     def test_scalar_at_ephemeris_end(self):
