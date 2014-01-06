49c189a597556fdf96efdbff7f9d2db0f4987944
Brandon Rhodes <brandon@rhodesmill.org>
2014-01-06 12:50:24 -0500

    Add project repository URL to ephemeris setup.py's

--- de421/setup.py
+++ de421/setup.py
@@ -11,6 +11,7 @@ setup(name = name,
       license = 'MIT',
       author = 'Brandon Rhodes',
       author_email = 'brandon@rhodesmill.org',
+      url = 'https://github.com/brandon-rhodes/python-jplephem/',
       classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
