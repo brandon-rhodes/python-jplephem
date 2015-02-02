6d771f7f301b3db86055c601a65b15a69cb88e94
Brandon Rhodes <brandon@rhodesmill.org>
2015-02-02 04:03:32 -0500

    Correctly parse the first summary in the file

--- daflib.py
+++ daflib.py
@@ -3,11 +3,13 @@
 http://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/req/daf.html
 
 """
-import struct
 import mmap
+import struct
+from pprint import pprint
 
-FTPSTR = 'FTPSTR:\r:\n:\r\n:\r\x00:\x81:\x10\xce:ENDFTP'  # FTP test string
 BFF = 'BIG-IEEE', 'LTL-IEEE', 'VAX-GFLT', 'VAX-DFLT'      # Binary file format
+FTPSTR = 'FTPSTR:\r:\n:\r\n:\r\x00:\x81:\x10\xce:ENDFTP'  # FTP test string
+RECORD_LENGTH = 1024
 
 class DAF(object):
     def __init__(self, open_file):
@@ -25,22 +27,50 @@ class DAF(object):
         else:
             raise ValueError('unrecognized format: {0!r}'.format(self.locfmt))
 
-        (self.locidw, self.nd, self.ni, self.locifn, self.fward, self.bward,
+        (locidw, self.nd, self.ni, locifn, self.fward, self.bward,
          self.free) = struct.unpack(self.endian + '8sII60sIII', map[:88])
 
-        if self.locidw != b'DAF/SPK ':
+        self.ss = self.nd + (self.ni + 1) // 2
+        self.summary_step = 8 * self.ss
+        self.summary_format = self.endian + 'd' * self.nd + 'i' * self.ni
+        self.summary_length = struct.calcsize(self.summary_format)
+
+        self.locidw = locidw.upper().rstrip()
+        self.locifn = locifn.upper().rstrip()
+
+        if self.locidw != b'DAF/SPK':
             raise ValueError(
                 'the first bytes do not identify this as a DAF/SPK file')
 
-        print vars(self)
+        if map[500:1000].strip(b'\0') != FTPSTR:
+            raise ValueError('the file has been damaged')
+
+        n = self.fward - 1
+        i = RECORD_LENGTH * n
+
+        summary_record = map[i:i + RECORD_LENGTH]
+        name_record = map[i + RECORD_LENGTH:i + 2 * RECORD_LENGTH]
+
+        next_summary, previous_summary, n_summaries = struct.unpack(
+            self.endian + 'ddd', summary_record[:24])
+
+        n_summaries = int(n_summaries)
+        print n_summaries
+
+        step = self.summary_step
+        length = self.summary_length
+
+        for i in range(0, n_summaries * step, step):
+            print name_record[i:i+step],
+            j = 24 + i
+            print struct.unpack(self.summary_format, summary_record[j:j+length])
+
+        pprint(vars(self))
 
 
 def main():
     with open('jup310.tmp', 'rb') as f:
         daf = DAF(f)
-
-    # if b[500:1000].strip(b'\0') != FTPSTR:
-    #     raise ValueError('the file has been damaged')
 
     # print repr(b[128:128+32])
     # print b.index('LTL-IEEE')
