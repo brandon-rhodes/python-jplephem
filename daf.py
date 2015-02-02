456980184f8e62b41faf2481159c95242ffffc3a
Brandon Rhodes <brandon@rhodesmill.org>
2015-02-02 03:21:48 -0500

    Switch from slow file I/O to an mmap

--- daf.py
+++ daf.py
@@ -4,21 +4,19 @@ http://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/req/daf.html
 
 """
 import struct
+import mmap
 
 FTPSTR = 'FTPSTR:\r:\n:\r\n:\r\x00:\x81:\x10\xce:ENDFTP'  # FTP test string
 BFF = 'BIG-IEEE', 'LTL-IEEE', 'VAX-GFLT', 'VAX-DFLT'      # Binary file format
 
 class DAF(object):
     def __init__(self, open_file):
-        self.f = open_file
+        self.map = mmap.mmap(open_file.fileno(), 0, access=mmap.ACCESS_READ)
         self.read_file_record()
 
     def read_file_record(self):
-        f = self.f
-        f.seek(0)
-        record = f.read(1024)
-
-        self.locfmt = record[88:96]
+        map = self.map
+        self.locfmt = map[88:96]
 
         if self.locfmt == b'BIG-IEEE':
             self.endian = '>'
@@ -28,7 +26,7 @@ class DAF(object):
             raise ValueError('unrecognized format: {0!r}'.format(self.locfmt))
 
         (self.locidw, self.nd, self.ni, self.locifn, self.fward, self.bward,
-         self.free) = struct.unpack(self.endian + '8sII60sIII', record[:88])
+         self.free) = struct.unpack(self.endian + '8sII60sIII', map[:88])
 
         if self.locidw != b'DAF/SPK ':
             raise ValueError(
