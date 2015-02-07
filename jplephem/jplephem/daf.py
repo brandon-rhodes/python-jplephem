19a4c3eff1b943aa6e07fedbbe6d9e986efe2643
Brandon Rhodes <brandon@rhodesmill.org>
2015-02-07 12:49:00 -0500

    Add DAF comments() method for reading comment area

--- jplephem/jplephem/daf.py
+++ jplephem/jplephem/daf.py
@@ -7,21 +7,19 @@ import mmap
 import struct
 import sys
 
-BFF = b'BIG-IEEE', b'LTL-IEEE', b'VAX-GFLT', b'VAX-DFLT'  # Binary file formats
 FTPSTR = b'FTPSTR:\r:\n:\r\n:\r\x00:\x81:\x10\xce:ENDFTP' # FTP test string
-RECORD_LENGTH = 1024
+K = 1024
 
 class DAF(object):
     """Access to NASA SPICE Double Precision Array Files (DAF)."""
 
     def __init__(self, path):
         with open(path, 'rb') as f:
-            m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            self.map = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
         if sys.version_info > (3,):
-            m = memoryview(m)
-        self.map = m
+            self.map = memoryview(self.map)
 
-        self.locfmt = m[88:96]
+        self.locfmt = self.map[88:96]
         if self.locfmt == b'BIG-IEEE':
             self.endian = '>'
         elif self.locfmt == b'LTL-IEEE':
@@ -30,7 +28,7 @@ class DAF(object):
             raise ValueError('unsupported format {0!r}'.format(self.locfmt))
 
         (locidw, nd, ni, locifn, self.fward, self.bward, self.free
-         ) = struct.unpack(self.endian + '8sII60sIII', m[:88])
+         ) = struct.unpack(self.endian + '8sII60sIII', self.map[:88])
 
         summary_size = nd + (ni + 1) // 2
         self.summary_step = 8 * summary_size
@@ -39,20 +37,32 @@ class DAF(object):
 
         self.locidw = locidw.upper().rstrip()
         self.locifn = locifn.upper().rstrip()
-        if self.locidw != b'DAF/SPK':
-            raise ValueError('the first file bytes do not specify "DAF/SPK"')
+        if not self.locidw.startswith(b'DAF/'):
+            raise ValueError('the file does not start with the 4 bytes "DAF/"')
 
-        if bytes(m[500:1000]).strip(b'\0') != FTPSTR:
+        if bytes(self.map[500:1000]).strip(b'\0') != FTPSTR:
             raise ValueError('this SPK file has been damaged')
 
     def bytes(self, start, stop):
-        """Return data from word `start` to `stop`, inclusive."""
+        """Return data of words `start` to `stop` inclusive; indexed from 1."""
         return self.map[8 * start - 8 : 8 * stop]
 
     def record(self, n):
         """Return record `n` as bytes; records are indexed from 1."""
-        stop = RECORD_LENGTH * n
-        return self.map[stop - RECORD_LENGTH:stop]
+        stop = n * K
+        return self.map[stop - K:stop]
+
+    def comments(self):
+        record_numbers = range(2, self.fward)
+        if not record_numbers:
+            return ''
+        data = b''.join(bytes(self.record(n)[0:1000]) for n in record_numbers)
+        try:
+            return data[:data.find(b'\4')].decode('ascii').replace('\0', '\n')
+        except IndexError:
+            raise ValueError('DAF file comment area is missing its EOT byte')
+        except UnicodeDecodeError:
+            raise ValueError('DAF file comment area is not ASCII text')
 
     def summaries(self):
         """Yield (name, (value, value, ...)) for each summary in the file."""
