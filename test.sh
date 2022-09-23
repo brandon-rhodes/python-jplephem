ea488c41602fc099d5d33ba697a2f364529e0a8e
Brandon Rhodes <brandon@rhodesmill.org>
2022-09-23 14:19:41 -0400

    Fix Python 2.7 in CI by naming module, not file

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ PYTHON=${PYTHON:-python}
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ci
-PYTHONPATH=$(dirname $PWD) $PYTHON -m unittest test.py "$@"
+PYTHONPATH=$(dirname $PWD) $PYTHON -m unittest test "$@"
