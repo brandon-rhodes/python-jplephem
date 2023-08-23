2c755adc313b36f1d40ddc2ed53526b08c31b1e0
Brandon Rhodes <brandon@rhodesmill.org>
2023-08-23 05:40:42 -0400

    Get old doctest.sh script working again

--- bin/doctest.sh
+++ bin/doctest.sh
@@ -2,4 +2,6 @@
 
 cd $(dirname "$0")
 cd ..
-python3 -m doctest -o ELLIPSIS /dev/stdin < jplephem/__init__.py
+cd ci
+export PYTHONPATH=..
+python3 -m doctest -o ELLIPSIS /dev/stdin < ../jplephem/__init__.py
