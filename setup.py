81059c3d44c3528dcd6337daa4baa14cbe4ada14
Antonio Valentino <antonio.valentino@tiscali.it>
2023-08-25 18:17:02 +0200

    Revert "Move project metadata to pyproject.toml"
    This reverts commit f4ebc5d4521527d440bf0e926ddb7b3996df3699.

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 from setuptools import setup
 
-import os, sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+# Fake the presence of numpy so import can succeed.
+import sys
+sys.modules['numpy'] = sys.modules['sys']
 
 import jplephem
 description, long_description = jplephem.__doc__.split('\n', 1)
 
-setup(
-    description = description,
-    long_description = long_description,
-)
+setup(name = 'jplephem',
+      version = jplephem.__version__,
+      description = description,
+      long_description = long_description,
+      license = 'MIT',
+      author = 'Brandon Rhodes',
+      author_email = 'brandon@rhodesmill.org',
+      url = 'https://github.com/brandon-rhodes/python-jplephem/',
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        ],
+      packages = ['jplephem'],
+      install_requires = ['numpy'],
+      )
